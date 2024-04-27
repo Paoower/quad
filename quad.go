02bf7462efc43497b3fa83fe9b1765196d3d749b
lmarchet <lucasmarchetti21@gmail.com>
2024-04-27 12:57:45 +0200

    quad optimization

--- quad.go
+++ quad.go
@@ -4,81 +4,70 @@ import "github.com/01-edu/z01"
 
 func QuadA(x, y int) {
 	// print first line
-	PrintFirstLine(x, 'o', 'o', '-')
+	PrintLine(x, 'o', 'o', '-')
 	// print intermediate lines
 	for i := 0; i < y-2; i++ {
 		PrintIntermediateLine(x, '|')
 	}
 	// print last line
 	if y > 1 {
-		PrintLastLine(x, 'o', 'o', '-')
+		PrintLine(x, 'o', 'o', '-')
 	}
 }
 
 func QuadB(x, y int) {
 	// print first line
-	PrintFirstLine(x, '/', 92, '*')
+	PrintLine(x, '/', 92, '*')
 	// print intermediate lines
 	for i := 0; i < y-2; i++ {
 		PrintIntermediateLine(x, '*')
 	}
 	// print last line
 	if y > 1 {
-		PrintLastLine(x, 92, '/', '*')
+		PrintLine(x, 92, '/', '*')
 	}
 }
 
 func QuadC(x, y int) {
 	// print first line
-	PrintFirstLine(x, 'A', 'A', 'B')
+	PrintLine(x, 'A', 'A', 'B')
 	// print intermediate lines
 	for i := 0; i < y-2; i++ {
 		PrintIntermediateLine(x, 'B')
 	}
 	// print last line
 	if y > 1 {
-		PrintLastLine(x, 'C', 'C', 'B')
+		PrintLine(x, 'C', 'C', 'B')
 	}
 }
 
 func QuadD(x, y int) {
 	// print first line
-	PrintFirstLine(x, 'A', 'C', 'B')
+	PrintLine(x, 'A', 'C', 'B')
 	// print intermediate lines
 	for i := 0; i < y-2; i++ {
 		PrintIntermediateLine(x, 'B')
 	}
 	// print last line
 	if y > 1 {
-		PrintLastLine(x, 'A', 'C', 'B')
+		PrintLine(x, 'A', 'C', 'B')
 	}
 }
 
 func QuadE(x, y int) {
 	// print first line
-	PrintFirstLine(x, 'A', 'C', 'B')
+	PrintLine(x, 'A', 'C', 'B')
 	// print intermediate lines
 	for i := 0; i < y-2; i++ {
 		PrintIntermediateLine(x, 'B')
 	}
 	// print last line
 	if y > 1 {
-		PrintLastLine(x, 'C', 'A', 'B')
+		PrintLine(x, 'C', 'A', 'B')
 	}
 }
 
-func PrintFirstLine(x int, firstEdge, lastEdge, inter rune) {
-	z01.PrintRune(firstEdge)
-	if x > 1 {
-		for i := 0; i < x-2; i++ {
-			z01.PrintRune(inter)
-		}
-		z01.PrintRune(lastEdge)
-	}
-	z01.PrintRune('\n')
-}
-
-func PrintLastLine(x int, firstEdge, lastEdge, inter rune) {
+func PrintLine(x int, firstEdge, lastEdge, inter rune) {
 	z01.PrintRune(firstEdge)
 	if x > 1 {
 		for i := 0; i < x-2; i++ {
